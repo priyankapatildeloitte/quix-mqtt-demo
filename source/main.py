186748cc2073ee4954a9551660975235b240e1e2
Quix Portal <developers+portal@quix.ai>
2021-04-01 15:15:33 +0000

    'source/main.py' edited via Quix Portal

--- source/main.py
+++ source/main.py
@@ -1,27 +1,14 @@
-# -*- coding: utf-8 -*-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
-import pandas as pd
-from flask import request
 import requests
 import io
 import datetime
-from dash.dependencies import Input, Output
 import dash  # (version 1.12.0)
 from dash.dependencies import Input, Output
-import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 import traceback
-from signalrcore.hub_connection_builder import HubConnectionBuilder
-from signalrcore.protocol.messagepack_protocol import MessagepackProtocol
+
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
